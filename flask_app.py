744f250d44361c43c0515cfd9e115d02e4e9f659
97marcar <hans.hansson22@gmail.com>
2015-12-15 09:06:50 +0100

    test

--- flask_app.py
+++ flask_app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 @app.route('/startpage/')
 def startpage():
     """..."""
-    
-    #flash("This site uses cookies")
+    flash("This site uses cookies")
     return render_template("startpage.html")
 
 if (__name__ == "__main__"):
