b1ffa1ce96a1dff48f9e14e9d5f5b71c5cb3db9d
97marcar <hans.hansson22@gmail.com>
2016-01-19 18:22:22 +0100

    some changes

--- database/ui.py
+++ database/ui.py
@@ -21,28 +21,28 @@ def ui():
     print("0. Exit")
     
     choice = input("Choose option: ")
-    if choice == "1":
+    if choice == "1" or choice == 1:
         create_product_table.create_table(create_product_table.db_name, "Product", create_product_table.sql)
         ui()
-    elif choice == "2":
+    elif choice == "2" or choice == 2:
         inserted_name = input("Name product: ")
         inserted_price = float(input("Price: "))
         values_to_insert = (inserted_name, inserted_price)
         insert_product_data.insert_data(values_to_insert)
         ui()
-    elif choice == "3":
+    elif choice == "3" or choice == 3:
         updated_name = input("New Name: ")
         updated_price = float(input("New Price: "))
         id_where_updated = int(input("Choose which ID that will be updated: "))
         tuple_to_be_used_to_update = (updated_name,updated_price,id_where_updated)
         update_data.update_product(tuple_to_be_used_to_update)
         ui()
-    elif choice == "4":
+    elif choice == "4" or choice == 4:
         deleted_name = input("Name of Product to be deleted: ")
         delete = (deleted_name,)
         delete_data.delete_product(delete)
         ui()
-    elif choice == "5":
+    elif choice == "5" or choice == 5:
         selected_id = int(input("Enter ID of the item you wish to select: "))
         select_existing_products.select_product(selected_id)
         ui()
